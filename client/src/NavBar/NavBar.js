fa71144fae30193222d6731503e4ddb8c3ef7d61
ChrisKim <ckimchris@Chris-Kims-MacBook-Pro-2.local>
2017-12-06 11:22:17 -0800

    navbar first draft

--- client/src/NavBar/NavBar.js
+++ client/src/NavBar/NavBar.js
@@ -3,28 +3,42 @@ import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import FlatButton from 'material-ui/FlatButton';
-
-function handleClick() {
-  alert('onClick triggered on the title component');
-}
-
-const styles = {
-  title: {
-    cursor: 'pointer',
-  },
-};
+import SearchBar from './Search';
+import DrawerOpenRightExample from './Drawer';
 
 /**
  * This example uses an [IconButton](/#/components/icon-button) on the left, has a clickable `title`
  * through the `onClick` property, and a [FlatButton](/#/components/flat-button) on the right.
  */
-const NavBar = () => (
-  <AppBar
-    title={<span style={styles.title}>Title</span>}
-    onTitleClick={handleClick}
-    iconElementLeft={<IconButton><NavigationClose /></IconButton>}
-    iconElementRight={<div><FlatButton label="Save" /><FlatButton label="Load" /></div>}
-  />
-);
+
+
+class NavBar extends React.Component {
+
+  onChange = () => {
+    
+  };
+    
+  onRequestSearch = () => {
+  
+  };
+
+  render() {
+    return (
+    <AppBar
+      style={{ 'backgroundColor': 'white' }}
+      title={<SearchBar onChange={this.onChange} onRequestSearch={this.onRequestSearch}/>}
+      titleStyle={{ 'paddingTop': '15px', 'paddingRight': '200px', 'paddingLeft': '10px' }}
+      iconElementRight={<div>
+        <FlatButton label="Sign Up" />
+        <FlatButton label="Log In" />
+      </div>}
+      iconStyleRight={{ 'marginTop': '22px' }}
+    />
+    )}
+};
 
 export default NavBar;
+
+/*
+      iconElementLeft={<IconButton><NavigationClose /></IconButton>}
+      iconStyleLeft={{ 'marginTop': '15px'}}
