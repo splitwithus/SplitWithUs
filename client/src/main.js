201eba186da18868e3cee70baed19a69fdf03a2d
brandondanh <brandondanh+github@gmail.com>
2017-12-06 11:37:38 -0800

    merging

--- client/src/main.js
+++ client/src/main.js
@@ -2,14 +2,10 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import NavBar from './NavBar/NavBar';
-<<<<<<< HEAD
 import AutoCompleteExampleSimple from './NavBar/Search';
 import DrawerOpenRightExample from './NavBar/Drawer';
 import Paper from './NavBar/Paper';
 import HomeContainer from './containers/HomeContainer';
-=======
-import ServicesContainer from './containers/ServicesContainer';
->>>>>>> a8b83850423be7d73224bc0c894acc027d3799b1
 
 import {
   BrowserRouter as Router,
@@ -26,15 +22,10 @@ render(
     <MuiThemeProvider>
       <div>
         <NavBar />
-<<<<<<< HEAD
         <AutoCompleteExampleSimple />
         <DrawerOpenRightExample />
         <Paper />
         <HomeContainer />
-=======
-        <ServicesContainer type={'Video'} services={['Netflix', 'Hulu']} />
-        <ServicesContainer type={'Music'} services={['Spotify', 'Pandora']} />
->>>>>>> a8b83850423be7d73224bc0c894acc027d3799b1
       </div>
     </MuiThemeProvider>
   </Provider>, document.getElementById('content')
